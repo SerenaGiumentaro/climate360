e96b9bbcf75a768003e5591892530df1744c0bf1
SerenaGiumentaro <serena.giumentaro@gmail.com>
2023-08-31 12:46:46 +0200

    adding desciptions

--- src/app/services/methane.service.ts
+++ src/app/services/methane.service.ts
@@ -13,7 +13,8 @@ export class MethaneService {
   constructor(private http: HttpClient) {}
   methaneData: ClimateActiveData = createClimateActiveObj(
     'Methane',
-    'methane dexcription diofv oksonfodjfdmfodbs',
+    `
+    Methane, is a potent greenhouse gas that significantly contributes to global warming. Its warming potential is over 25 times greater than CO2 over a 100-year period. Produced by natural sources like wetlands, as well as human activities such as livestock digestion, rice cultivation, and fossil fuel extraction, methane's rise in the atmosphere exacerbates the greenhouse effect. As the Earth warms, permafrost regions begin to thaw, releasing trapped methane and creating a dangerous feedback loop of increasing temperatures. This not only accelerates the melting of polar ice caps and the rise of sea levels but also leads to more extreme weather events, from intense heatwaves to heavy rainfall and flooding. Marine ecosystems, too, are affected, with warmer waters threatening coral reefs and the species that rely on them. The socio-economic consequences are vast, with communities facing threats to their homes, livelihoods, and food sources. Addressing the methane challenge is imperative, necessitating global efforts to reduce emissions from both natural and anthropogenic sources.`,
     '#ba5202',
     'Year',
     'Part Per million (ppm)',
